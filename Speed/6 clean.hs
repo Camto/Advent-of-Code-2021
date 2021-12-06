bcbf81b773ddd1bdf031b01a4dba3900b54ad996
Camto <www.ben.pr@gmail.com>
2021-12-06 03:53:29 -0400

    Just felt this one would look nice.

---
+++ Speed/6 clean.hs
@@ -0,0 +1,15 @@
+import Data.List
+
+step [a,b,c,d,e,f,g,h,i] = [b,c,d,e,f,g,h + a,i,a]
+
+nSteps n = (!! n) . iterate step
+
+count n = length . filter (== n)
+
+main = do
+	input <- readFile "input.txt"
+	let nums = read $ "[" ++ input ++ "]" :: [Integer]
+	let fish = map ((flip count) nums) [0..8]
+	
+	print . sum $ nSteps 80 fish -- Part 1
+	print . sum $ nSteps 256 fish -- Part 2
